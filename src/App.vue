89a0bed3b9423695bcd4e93a571abb027764c761
JGC-HUST <a1344143728@163.com>
2019-06-06 20:52:38 +0800

    update02

--- src/App.vue
+++ src/App.vue
@@ -1,36 +1,32 @@
 <template>
-  <div id="app">
-    <router-link to='/home'>Home</router-link>
-    <router-link to="/about">About</router-link>
-    <router-view></router-view>
-
-  </div>
+	<div id="app">
+		<router-view></router-view>
+	</div>
 </template>
 
 <script>
-import zh_CN from 'ant-design-vue/lib/locale-provider/zh_CN'
-import HelloWorld from './components/HelloWorld.vue'
+import zh_CN from "ant-design-vue/lib/locale-provider/zh_CN";
+import HelloWorld from "./components/HelloWorld.vue";
 
 export default {
-  name: 'app',
-  data() {
-    return {
-      zh_CN,
-    }
-  },
-  components: {
-    HelloWorld
-  }
-}
+	name: "app",
+	data() {
+		return {
+			zh_CN
+		};
+	},
+	components: {
+		HelloWorld
+	}
+};
 </script>
 
 <style>
 #app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+	font-family: "Avenir", Helvetica, Arial, sans-serif;
+	-webkit-font-smoothing: antialiased;
+	-moz-osx-font-smoothing: grayscale;
+	text-align: center;
+	color: #2c3e50;
 }
 </style>
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,21 +1,56 @@
 <template>
 	<div class="home">
-		<!-- particles.js container -->
-		<div id="particles-js"></div>
-		<!-- stats - count particles -->
-		<div class="count-particles"><span class="js-count-particles">--</span> particles</div>
-		<!-- particles.js lib (JavaScript CodePen settings): https://github.com/VincentGarreau/particles.js -->
+		<div id="particles"></div>
+		<div class=""></div>
+		<div class="form"></div>
 	</div>
 </template>
 
 <script>
 // @ is an alias to /src
 import HelloWorld from "@/components/HelloWorld.vue";
+import particlesJs from "particles.js";
+import particlesConfig from "./config.json";
 
 export default {
 	name: "home",
 	components: {
 		HelloWorld
+	},
+	mounted() {
+		particlesJS("particles", particlesConfig);
 	}
 };
 </script>
+
+<style scoped>
+/* ---- reset ---- */
+
+body {
+	margin: 0;
+	font: normal 75% Arial, Helvetica, sans-serif;
+}
+
+canvas {
+	display: block;
+}
+
+/* ---- particles.js container ---- */
+
+#particles {
+	position: absolute;
+	width: 100%;
+	height: 100%;
+	background-color: #69c0ff;
+	background-image: url("");
+	background-repeat: no-repeat;
+	background-size: cover;
+	background-position: 50% 50%;
+	z-index: -55;
+}
+.mask {
+	width: 100%;
+	height: 100%;
+	z-index: -5;
+}
+</style>
